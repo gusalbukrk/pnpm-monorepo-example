a8cb4fee6d2920be837783505f7cb07c845aadef
gusalbukrk <gusalbukrk@gmail.com>
2022-02-07 15:41:41 -0300

    initial commit

---
+++ packages/main/index.js
@@ -0,0 +1,8 @@
+import { reduce } from 'lodash-es';
+import { add as addBase, mul as mulBase } from 'math';
+
+const add = (arr) => reduce(arr, (prev, curr) => addBase(prev, curr));
+const mul = (arr) => reduce(arr, (prev, curr) => mulBase(prev, curr));
+
+console.log(add([3, 5, 8, 12])); // 28
+console.log(mul([3, 5, 8, 12])); // 1440
